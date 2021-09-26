7db008c175e1f12dbc59efec4d15c8d44193c024
ryanjyost <ryost217@gmail.com>
2021-09-26 08:23:01 -0500

    cleanup, redirects

--- src/screens/Loading.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { LoadingOutlined } from "@ant-design/icons";
-
-export default function Loading() {
-  return <LoadingOutlined style={{ fontSize: 50, color: "#1890ff" }} />;
-}
